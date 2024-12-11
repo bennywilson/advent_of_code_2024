2928f73daf14d50971a9f33cb5e4b0caca393552
Benjamin Wilson <35935536+bennywilson@users.noreply.github.com>
2024-12-11 10:27:56 -0500

    Day 9 progress

--- Day9_2.cpp
+++ Day9_2.cpp
@@ -1,7 +1,6 @@
 #include <chrono>
 #include <fstream>
 #include <iostream>
-#include <list>
 #include <map>
 #include <sstream>
 #include <unordered_set>
@@ -14,7 +13,7 @@ using namespace std::chrono;
  *
  */
 int main() {
-	const auto start = high_resolution_clock::now();
+	const auto start_time = high_resolution_clock::now();
 	ifstream file("input.txt");
 	if (!file.is_open()) {
 		return 0;
@@ -33,29 +32,20 @@ int main() {
 			int index;
 			int num_free;
 		};
-		list<FreeBlockInfo> free_blocks;
+
 		vector<uint16_t> disk_map;
 		for (uint32_t i = 0; i < line.size(); i++) {
 			uint16_t output = UINT16_MAX;
 			const uint32_t count = line[i] - '0';
 			if (!(i & 0x01)) {
 				output = i >> 1;
-			} else {
-				FreeBlockInfo new_block(disk_map.size(), count);
-				free_blocks.push_back(new_block);
 			}
-
 			for (uint32_t j = 0; j < count; j++) {
 				disk_map.push_back(output);
 			}
 		}
 
-		for (auto it = free_blocks.begin(); it != free_blocks.end(); ++it) {
-			cout << it->index << " - " << it->num_free << endl;
-		}
-
-		size_t start = 0;
-		size_t end = disk_map.size() - 1;
+		int64_t end = disk_map.size() - 1;
 		const size_t max_size = (size_t)-1;
 		for (auto it = disk_map.begin(); it != disk_map.end(); ++it) {
 			if (*it == UINT16_MAX) {
@@ -66,20 +56,56 @@ int main() {
 			}
 		}
 
-		while (start <= end) {
-			while (start < disk_map.size() && disk_map[start] != UINT16_MAX) {
-				start++;
-			}
+		while (end > 0) {
 
-			while (end != max_size && disk_map[end] == UINT16_MAX) {
-				end--;
+		    
+		      while (end != max_size && disk_map[end] == UINT16_MAX) {
+		        end--;
 			}
-
-			if (end < max_size && start < end) {
-				const uint16_t swap_val = disk_map[start];
-				disk_map[start] = disk_map[end];
-				disk_map[end] = swap_val;
+		    size_t run_val = disk_map[end];
+			
+            size_t end_start = end;
+            while (end_start != max_size && disk_map[end_start] != UINT16_MAX && disk_map[end_start] == run_val) {
+		        end_start--;
 			}
+			if (end_start == max_size) {
+			    end_start = 0;
+			}
+			
+            const int needed_size = end - end_start;
+            cout << "Needed size is " << needed_size << endl;
+            size_t start = 1;
+            size_t run_length = 1;
+            bool block_found = false;
+            while (start < end) {
+                if (disk_map[start + run_length] != UINT16_MAX) {
+                    start += run_length;
+                    run_length = 1;
+                } else {
+        
+            
+                    if (run_length >= needed_size) {
+                        block_found = true;
+                        cout << "Block found at " << start << " run length is " << run_length << endl;
+                        for (int i = 0; i < run_length; i++) {
+                            disk_map[start+i+1] = disk_map[end_start + i + 1];
+                            disk_map[end_start + i + 1] = UINT16_MAX;
+                        }
+                        break;
+                    }
+                                run_length++;
+                }
+            }
+            cout << "Block found = " << block_found << endl;
+	end = end_start;
+      		for (auto it = disk_map.begin(); it != disk_map.end(); ++it) {
+    			if (*it == UINT16_MAX) {
+    				cout << ".";
+    			} else {
+    				cout << *it;
+    			}
+    		}
+    		cout << endl;
 		}
 
 
