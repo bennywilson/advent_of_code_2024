580e39e06d807bdb2f3a20c2bd68482bdce023e9
Benjamin Wilson <35935536+bennywilson@users.noreply.github.com>
2024-12-19 16:19:08 -0500

    Cleaned up and gtg

--- 19/main.cpp
+++ 19/main.cpp
@@ -3,13 +3,12 @@
 #include <iostream>
 #include <unordered_map>
 #include <unordered_set>
-#include <queue>
 #include <sstream>
-#include <windows.h>
-#include <conio.h>
 
 using namespace std;
 using namespace std::chrono;
+
+bool g_log = false;
 
 template<typename V,typename T>
 bool std_find(const vector<V>& vec, const T& value) {
@@ -21,10 +20,7 @@ bool std_find(const V& vec, const T& value) {
 	return vec.find(value) != vec.end();
 }
 
-
-string tabs;
-
-bool has_match(string design, unordered_set<string>& patterns, unordered_map<string, bool>& cache) {
+bool has_match(string design, unordered_set<string>& patterns, unordered_map<string, bool>& cache, string& tabs) {
 	if (std_find(cache, design)) {
 		return cache[design];
 	}
@@ -42,18 +38,14 @@ bool has_match(string design, unordered_set<string>& patterns, unordered_map<str
 		string second = design.substr(l);
 
 		tabs += "    ";
-		if (has_match(first, patterns, cache) && has_match(second, patterns, cache)) {
-			tabs.pop_back();
-			tabs.pop_back();
-			tabs.pop_back();
+		if (has_match(first, patterns, cache, tabs) && has_match(second, patterns, cache, tabs)) {
+			tabs.resize(tabs.size() - 3);
 			cache[design] = true;
 			return true;
 		}
 	}
 
-	tabs.pop_back();
-	tabs.pop_back();
-	tabs.pop_back();
+	tabs.resize(tabs.size() - 3);
 	cache[design] = false;
 	return false;
 }
@@ -76,7 +68,6 @@ void part_one() {
 				pattern = pattern.substr(0, pattern.length() - 1);
 			}
 			if (!std_find(patterns, pattern)) {
-				//cout << "Pushing " << pattern << endl;
 				patterns.push_back(pattern);
 				pattern_set.insert(pattern);
 			}
@@ -88,50 +79,46 @@ void part_one() {
 		designs.push_back(line);
 	}
 
-	cout << "\nPatterns: ";
-	for (size_t i = 0; i < patterns.size(); i++) {
-		cout << patterns[i] << " ";
+	if (g_log) {
+		cout << "\nPatterns: ";
+		for (size_t i = 0; i < patterns.size(); i++) {
+			cout << patterns[i] << " ";
+		}
+
+		cout << "\n\nDesigns:\n";
+		for (size_t i = 0; i < designs.size(); i++) {
+			cout << designs[i] << " ";
+		}
 	}
 
-	cout << "\n\nDesigns:\n";
-	for (size_t i = 0; i < designs.size(); i++) {
-		cout << designs[i] << endl;
+	if (g_log) {
+		cout << "\n\n\nRunning program_____" << endl;
 	}
 
 	uint64_t num_found = 0;
 	for (int i = 0; i < designs.size(); i++) {
 		cout << designs[i] << " ";
 		unordered_map<string, bool> cache;
-		if (has_match(designs[i], pattern_set, cache)) {
-			num_found++;
-			//cout << " was found!";
+		string tabs;
+		if (has_match(designs[i], pattern_set, cache, tabs)) {
+			num_found++;	
+			if (g_log) {
+				cout << "	Design " << designs[i] << "has a match" << endl << endl;
+			}
 		}
-		else {
-		//	cout << " was not found!";
-		}
-
-	//	cout << endl;
 	}
-
-	cout << "Num found is " << num_found << endl;
 }
 
-int count_matches(string design, const uint64_t cur_idx, unordered_set<string>& patterns, unordered_map<string, bool>& cache) {
-	/*if (map_contains(cache, design)) {
+uint64_t count_matches(string design, const uint64_t cur_idx, unordered_set<string>& patterns, unordered_map<string, uint64_t>& cache, string& tabs) {
+	if (std_find(cache, design)) {
+		if (cache[design] > 0) {
+			if (g_log) {
+				cout << tabs << " Cached design " << design << " match found! " << endl;
+			}
+		}
 		return cache[design];
-	}*/
-
-	/*cout << tabs << "Design: " << design << endl;
-	if (design.size() == 0) {
-		cout << tabs << "empty design gtg!\n";
-		return 1;
 	}
 
-	if (std_contains(patterns, design)) {
-		cout << tabs << "	match found\n";
-		return 1;
-	}
-*/
 	uint64_t count = 0;
 	for (size_t i = 1; i < design.size(); i++) {
 		const string first = design.substr(0, i);
@@ -141,20 +128,27 @@ int count_matches(string design, const uint64_t cur_idx, unordered_set<string>& 
 
 		const string second = design.substr(i);
 		if (std_find(patterns, second)) {
+			if (g_log) {
+				cout << tabs << "Match found!" << endl;
+			}
 			count++;
 		}
 
-		cout << tabs << first << ", " << second << endl;
+		if (g_log) {
+			cout << tabs << first << ", " << second << endl;
+		}
+
 		tabs += "   ";
-		count += count_matches(second, 0, patterns, cache);
-	/*	tabs.pop_back();
-		tabs.pop_back();
-		tabs.pop_back();*/
+		count += count_matches(second, 0, patterns, cache, tabs);
+		tabs.resize(tabs.size() - 3);
 	}
 
-	tabs.pop_back();
-	tabs.pop_back();
-	tabs.pop_back();
+	cache[design] = count;
+	//tabs.resize(tabs.size() - 3);
+
+	if (g_log && design.size() == 1) {
+		cout << endl;
+	}
 
 	return count;
 }
@@ -177,7 +171,6 @@ void part_two() {
 				pattern = pattern.substr(0, pattern.length() - 1);
 			}
 			if (!std_find(patterns, pattern)) {
-				//cout << "Pushing " << pattern << endl;
 				patterns.push_back(pattern);
 				pattern_set.insert(pattern);
 			}
@@ -188,35 +181,15 @@ void part_two() {
 	while (std::getline(file, line)) {
 		designs.push_back(line);
 	}
-	unordered_map<string, bool> cache;
-	int a = count_matches(designs[0], 0, pattern_set, cache);
-	cout << a;
-	/*cout << "\nPatterns: ";
-	for (size_t i = 0; i < patterns.size(); i++) {
-		cout << patterns[i] << " ";
+	unordered_map<string, uint64_t> cache;
+	uint64_t num_found = 0;
+
+	string tabs;
+	for (const auto& design: designs) {
+		num_found += count_matches(design, 0, pattern_set, cache, tabs);
 	}
 
-	cout << "\n\nDesigns:\n";
-	for (size_t i = 0; i < designs.size(); i++) {
-		cout << designs[i] << endl;
-	}
-
-		uint64_t num_found = 0;
-		for (int i = 0; i < designs.size(); i++) {
-			cout << designs[i] << " ";
-			unordered_map<string, bool> cache;
-			if (count_matches(designs[i], 0, pattern_set, cache)) {
-				num_found++;
-				cout << " was found!";
-			}
-			else {
-				cout << " was not found!";
-			}
-
-			cout << endl;
-		}
-
-	cout << "Num found is " << num_found << endl;*/
+	cout << "Num found is " << num_found << endl;
 }
 
 
@@ -224,46 +197,10 @@ void part_two() {
  *
  */
 int main() {
-//	cout << "Part one...\n";
-//	part_one();
+	cout << "Part one..........................................................\n";
+	part_one();
 
-	cout << "\n\nPart two ...\n";
+	cout << "\n\nPart two......................................................\n";
 	part_two();
 
 }
-
-// 278
-
-/*
-bggr
-  |
-  |-b		ggr				- 0
-  |			|-g		gr		- 0
-  |			|		|-r		- 1	return
-  |			|
-  | 		|-gg	r		- skip
-  |			|-ggr			- skip
-  |			|
-  |-bg		gr				- skip
-  |-bgg		r				- skip
-  |-bggr
-
-gbbr
- |
- |-g		bbr				- 0
- |			|-b		br		- 1
- |			|		|-r		- 1 return
- |			|
- |			|-bb	-r		- skip
- |			|-bbr			- skip
- |
- |-gb		br				- 1
- |			|-b		r		- 1 return
- |			|
- |			|-br			- 1 return
- |
- |- gbb						- skip
- |- gbbr					- skip
-
-
-* */
