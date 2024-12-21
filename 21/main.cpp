7d0cbd9c9b3f94404867faead08291631b350481
Benjamin Wilson <35935536+bennywilson@users.noreply.github.com>
2024-12-21 15:51:35 -0500

    Added bfs for Day 21

---
+++ 21/main.cpp
@@ -0,0 +1,146 @@
+﻿#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <queue>
+#include <fcntl.h>
+
+#include <io.h>
+#include <stdio.h>
+#include <windows.h>
+
+using namespace std;
+using namespace std::chrono;
+
+bool g_log = false;
+int64_t g_board_width = 0;
+int64_t g_board_height = 0;
+string g_board;
+
+template<typename V, typename T>
+bool std_contains(const vector<V>& vec, const T& value) {
+	return std::find(vec.begin(), vec.end(), value) != vec.end();
+}
+
+template<typename V, typename T>
+bool std_contains(const V& vec, const T& value) {
+	return vec.find(value) != vec.end();
+}
+
+struct Vec2 {
+	Vec2() :
+		x(0),
+		y(0) {}
+
+	Vec2(const int64_t in_x, const int64_t in_y) :
+		x(in_x),
+		y(in_y) {}
+
+	Vec2 operator+(const Vec2& op2) const {
+		return Vec2(x + op2.x, y + op2.y);
+	}
+
+	Vec2 operator-(const Vec2& op2) const {
+		return Vec2(x - op2.x, y - op2.y);
+	}
+
+	Vec2 operator*(const int64_t op2) const {
+		return Vec2(op2 * x, op2 * y);
+	}
+
+	Vec2 left() const { return *this - Vec2(1, 0); }
+	Vec2 right() const { return *this + Vec2(1, 0); }
+	Vec2 up() const { return *this - Vec2(0, 1); }
+	Vec2 down() const { return *this + Vec2(0, 1); }
+
+	bool operator==(const Vec2& op2) const {
+		return x == op2.x && y == op2.y;
+	}
+
+	bool operator!=(const Vec2& op2) const {
+		return x != op2.x || y != op2.y;
+	}
+
+	bool valid_numpad_pos() const {
+		if (x < 0 || x > 2 || y < 0 || y > 3 || (x == 0 && y ==3)) {
+			return false;
+		}
+		return true;
+	}
+
+	int64_t x;
+	int64_t y;
+};
+
+struct PathNode {
+	PathNode() : pos(-1, -1) {}
+	PathNode(const Vec2& _pos, const Vec2& prev_pos, const vector<Vec2>& prev_path = vector<Vec2>()) :
+		pos(_pos),
+		path_taken(prev_path) {
+			path_taken.push_back(prev_pos);
+		}
+
+	Vec2	pos;
+	vector<Vec2> path_taken;
+};
+
+/**
+ *
+ */
+int main() {
+	cout << "Part one..........................................................\n";
+	
+
+	const string input = "029A";
+/*
++---+---+---+
+| 7 | 8 | 9 |
++---+---+---+
+| 4 | 5 | 6 |
++---+---+---+
+| 1 | 2 | 3 |
++---+---+---+
+	| 0 | A |
+	+---+---+
+*/
+	const int64_t numpad_width = 3;
+	const int64_t numpad_height = 4;
+	const Vec2 invalid_numpad_pos(0, 3);
+
+	//vector<int64_t> 
+	const Vec2 start_pos(2, 3);
+	const Vec2 end_pos(1, 0);
+
+	queue<PathNode> q;
+	q.push(PathNode(start_pos, Vec2(-1, -1)));
+
+	while (!q.empty()) {
+		const PathNode cur_node = q.front();
+		q.pop();
+		if (cur_node.pos == end_pos) {
+			cout << "Found path!" << endl;
+			for (int i = 0; i < cur_node.path_taken.size(); i++) {
+				const Vec2 path_pos = Vec2(cur_node.path_taken[i]);
+				cout << path_pos.x << ", " << path_pos.y << endl;
+			}
+			continue;
+		}
+
+		const Vec2& cur_pos = cur_node.pos;
+		if (!cur_pos.valid_numpad_pos()) {
+			continue;
+		}
+
+		if (cur_pos.x < end_pos.x && cur_pos.right().valid_numpad_pos()) {
+			q.push(PathNode(cur_pos.right(), cur_pos, cur_node.path_taken));
+		} else if (cur_pos.x > end_pos.x && cur_pos.left().valid_numpad_pos()) {
+			q.push(PathNode(cur_pos.left(), cur_pos, cur_node.path_taken));
+		} else if (cur_pos.y < end_pos.y && cur_pos.down().valid_numpad_pos()) {
+			q.push(PathNode(cur_pos.down(), cur_pos, cur_node.path_taken));
+		} else if (cur_pos.up().valid_numpad_pos()) {
+			q.push(PathNode(cur_pos.up(), cur_pos, cur_node.path_taken));
+		}
+	}
+}
